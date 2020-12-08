3d5123d8479b8bee4b58924278080e9adf9fd20c
Jhonatan M. Nagasako <jhonatannagasako@gmail.com>
2020-12-08 06:38:45 -0500

    Data pull
    Text file pull to desktop via python

--- Project 1_Explore Weather Trends/v01_project 01_explore weather trends.py
+++ Project 1_Explore Weather Trends/v01_project 01_explore weather trends.py
@@ -1,3 +1,12 @@
+# Jhonatan Nagasako
+# Udacity Data Analysis
+# Project 1 - Analyzing Global Temps
+# Purpose:
+#      Using code and CSV files provide to determine
+#      moving averages and line graph comparisons
+
+############################
+
 import urllib
 dls = "http://www.muellerindustries.com/uploads/pdf/UW SPD0114.xls"
 urllib.request.urlretrieve(dls, "test.xls")  # For Python 3
